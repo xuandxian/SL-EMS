a7c76a6501397f25bf3bf0e55dcc5a6ac4c67729
Tony1213 <1973265001@qq.com>
2015-10-05 22:18:06 +0800

    欢迎界面
    创建欢迎界面

--- EMS/src/com/overtech/ems/MainActivity.java
+++ EMS/src/com/overtech/ems/MainActivity.java
@@ -4,10 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
---
+++ EMS/src/com/overtech/ems/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.overtech.ems;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+			}
+		}, 3000);
+	}
+
+}
