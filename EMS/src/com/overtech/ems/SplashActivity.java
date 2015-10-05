2c2c14b595c9a27f25b2a42faf6ccddd071e1afd
Tony1213 <1973265001@qq.com>
2015-10-05 22:30:00 +0800

    欢迎界面
    完善欢迎界面

--- EMS/src/com/overtech/ems/SplashActivity.java
+++ EMS/src/com/overtech/ems/SplashActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class SplashActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash);
 		new Handler().postDelayed(new Runnable() {
 
 			@Override
@@ -18,6 +20,7 @@ public class SplashActivity extends Activity {
 				Intent intent = new Intent(SplashActivity.this,
 						MainActivity.class);
 				startActivity(intent);
+				finish();
 			}
 		}, 3000);
 	}
