42afdda16109c6e0dfabe0ee7975a2500cbb3941
Tony1213 <1973265001@qq.com>
2015-10-09 11:14:53 +0800

    注册页面（学历和工作）
    添加注册界面学历和工作界面

---
+++ EMS/src/com/overtech/ems/RegisterAddPersonEduAndWorkActivity.java
@@ -0,0 +1,13 @@
+package com.overtech.ems;
+
+import android.os.Bundle;
+
+public class RegisterAddPersonEduAndWorkActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register_add_person_edu_and_work);
+	}
+
+}
