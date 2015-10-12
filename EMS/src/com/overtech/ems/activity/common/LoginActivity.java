3431bc48f6675d6cc582b80af4cdbcd801bde3cf
Tony1213 <1973265001@qq.com>
2015-10-12 14:34:57 +0800

    删除重复的views包，添加togglebutton密码状态
    删除重复的views包，添加togglebutton密码状态，修改密码Max长度为18位

--- EMS/src/com/overtech/ems/activity/common/LoginActivity.java
+++ EMS/src/com/overtech/ems/activity/common/LoginActivity.java
@@ -3,14 +3,20 @@ package com.overtech.ems.activity.common;
 import com.overtech.ems.R;
 import com.overtech.ems.activity.BaseActivity;
 import com.overtech.ems.activity.parttime.MainActivity;
+import com.overtech.views.EditTextWithDelete;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 /**
  * @author Tony
@@ -18,11 +24,13 @@ import android.widget.TextView;
  * @date 2015-10-05
  */
 public class LoginActivity extends BaseActivity {
+	private EditTextWithDelete mUserName, mPassword;
 	private TextView mHeadContent;
 	private ImageView mHeadBack;
 	private TextView mLostPassword;
 	private Button mLogin;
 	private TextView mRegister;
+	private ToggleButton mChangePasswordState;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +74,32 @@ public class LoginActivity extends BaseActivity {
 				onBackPressed();
 			}
 		});
+		mChangePasswordState
+		.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if (isChecked) {
+					//设置密码为可见的
+					mPassword.setTransformationMethod(HideReturnsTransformationMethod
+							.getInstance());
+				} else {
+					mPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+				}
+			}
+		});
 	}
 
 	private void findViewById() {
 		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
 		mHeadBack = (ImageView) findViewById(R.id.iv_headBack);
+		mUserName = (EditTextWithDelete) findViewById(R.id.et_login_username);
+		mPassword = (EditTextWithDelete) findViewById(R.id.et_login_password);
 		mLostPassword = (TextView) findViewById(R.id.tv_lost_password);
 		mRegister = (TextView) findViewById(R.id.tv_login_by_message);
 		mLogin = (Button) findViewById(R.id.btn_login);
+		mChangePasswordState = (ToggleButton) findViewById(R.id.tb_change_password);
 	}
 
 	private void init() {
