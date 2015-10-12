e92407d2713ce63301c129af834c33af19c12edf
Tony1213 <1973265001@qq.com>
2015-10-12 17:57:07 +0800

    修改底部按钮样式

--- EMS/src/com/overtech/ems/activity/parttime/MainActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/MainActivity.java
@@ -5,15 +5,18 @@ import com.overtech.ems.activity.parttime.fragment.GrabTaskFragment;
 import com.overtech.ems.activity.parttime.fragment.NearByFragment;
 import com.overtech.ems.activity.parttime.fragment.PersonalZoneFragment;
 import com.overtech.ems.activity.parttime.fragment.TaskListFragment;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -32,6 +35,14 @@ public class MainActivity extends Activity implements OnClickListener {
 	private RelativeLayout mRelNearBy;
 	private RelativeLayout mRelTaskList;
 	private RelativeLayout mRelPersonalZone;
+	private TextView mTabGrabTaskContent;
+	private TextView mTabNearByContent;
+	private TextView mTabTaskListContent;
+	private TextView mTabPersonalZoneContent;
+	private ImageView mTabGrabTaskIv;
+	private ImageView mTabNearByIv;
+	private ImageView mTabTaskListIv;
+	private ImageView mTabPersonalZoneIv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +59,14 @@ public class MainActivity extends Activity implements OnClickListener {
 		mRelNearBy = (RelativeLayout) findViewById(R.id.rl_parttime_nearby);
 		mRelTaskList = (RelativeLayout) findViewById(R.id.rl_parttime_task_list);
 		mRelPersonalZone = (RelativeLayout) findViewById(R.id.rl_parttime_personal_zone);
+		mTabGrabTaskContent = (TextView) findViewById(R.id.tv_grab_taskn);
+		mTabNearByContent = (TextView) findViewById(R.id.tv_nearby);
+		mTabTaskListContent = (TextView) findViewById(R.id.tv_task_list);
+		mTabPersonalZoneContent = (TextView) findViewById(R.id.tv_personal_zone);
+		mTabGrabTaskIv = (ImageView) findViewById(R.id.iv_grab_task);
+		mTabNearByIv = (ImageView) findViewById(R.id.iv_nearby);
+		mTabTaskListIv = (ImageView) findViewById(R.id.iv_task_list);
+		mTabPersonalZoneIv = (ImageView) findViewById(R.id.iv_personal_zone);
 	}
 
 	private void init() {
@@ -64,6 +83,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		mRelPersonalZone.setOnClickListener(this);
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public void onClick(View v) {
 		FragmentManager manager = getFragmentManager();
@@ -71,24 +91,36 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.rl_parttime_grab_task:
 			mHeadContent.setText("抢单");
+			mTabGrabTaskIv
+					.setBackgroundResource(R.drawable.shopping_home_tab_take_out_selected);
+			mTabGrabTaskContent.setTextColor(Color.argb(255, 255, 0, 255));
 			mGrabTaskFragment = new GrabTaskFragment();
 			transaction.replace(R.id.fragment_content, mGrabTaskFragment);
 			transaction.commit();
 			break;
 		case R.id.rl_parttime_nearby:
 			mHeadContent.setText("附近");
+			mTabNearByIv
+					.setBackgroundResource(R.drawable.shopping_home_tab_found_selected);
+			mTabNearByContent.setTextColor(Color.argb(255, 255, 0, 255));
 			mNearByFragment = new NearByFragment();
 			transaction.replace(R.id.fragment_content, mNearByFragment);
 			transaction.commit();
 			break;
 		case R.id.rl_parttime_task_list:
 			mHeadContent.setText("任务单");
+			mTabTaskListIv
+					.setBackgroundResource(R.drawable.shopping_home_tab_order_selected);
+			mTabTaskListContent.setTextColor(Color.argb(255, 255, 0, 255));
 			mTaskListFragment = new TaskListFragment();
 			transaction.replace(R.id.fragment_content, mTaskListFragment);
 			transaction.commit();
 			break;
 		case R.id.rl_parttime_personal_zone:
 			mHeadContent.setText("我的");
+			mTabPersonalZoneIv
+					.setBackgroundResource(R.drawable.shopping_home_tab_personal_selected);
+			mTabPersonalZoneContent.setTextColor(Color.argb(255, 255, 0, 255));
 			mPersonalZoneFragment = new PersonalZoneFragment();
 			transaction.replace(R.id.fragment_content, mPersonalZoneFragment);
 			transaction.commit();
