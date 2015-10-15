0001c3154e79657399b515c5c233d4e4dbeb9c9c
Tony1213 <1973265001@qq.com>
2015-10-15 10:19:09 +0800

    Fragment切换不重新实例化
    兼职附近模块

--- EMS/src/com/overtech/ems/activity/parttime/fragment/NearByFragment.java
+++ EMS/src/com/overtech/ems/activity/parttime/fragment/NearByFragment.java
@@ -11,58 +11,61 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class NearByFragment extends Fragment {
+public class NearByFragment extends Fragment implements OnClickListener {
 
 	private Button mNearByMapBtn;
 	private Button mNearByListBtn;
+	private View view;
+	private FragmentManager manager;
+	private FragmentTransaction transaction;
+	private Fragment mContent;
+	private Fragment mNearByMap;
+	private Fragment mNearByList;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater
-				.inflate(R.layout.fragment_nearby, container, false);
-		initMapView();
-		init(view);
-		initEvents();
+		view = inflater.inflate(R.layout.fragment_nearby, container, false);
+		init();
+		setDefaultView();
 		return view;
 	}
 
-	private void init(View view) {
+	private void init() {
 		mNearByMapBtn = (Button) view.findViewById(R.id.btn_nearby_title_map);
 		mNearByListBtn = (Button) view.findViewById(R.id.btn_nearby_title_list);
+		mNearByMapBtn.setOnClickListener(this);
+		mNearByListBtn.setOnClickListener(this);
+		manager = getFragmentManager();
+		mNearByMap = new NearByMapFragment();
+		mNearByList=new NearByListFragment();
 	}
 
-	private void initEvents() {
-		mNearByMapBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				initMapView();
-			}
-		});
-		mNearByListBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				initNearByListView();
-			}
-		});
-	}
-
-	private void initMapView() {
-		FragmentManager manager = getFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
-		Fragment mNearByMap = new NearByMapFragment();
-		transaction.replace(R.id.rl_nearby_content, mNearByMap);
+	private void setDefaultView() {
+		transaction = manager.beginTransaction();
+		transaction.replace(R.id.rl_nearby_content, mNearByMap, "mNearByMap");
 		transaction.commit();
 	}
 
-	private void initNearByListView() {
-		FragmentManager manager = getFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
-		Fragment mNearByList = new NearByListFragment();
-		transaction.replace(R.id.rl_nearby_content, mNearByList);
-		transaction.commit();
+	public void switchContent(Fragment from, Fragment to) {
+		transaction = manager.beginTransaction();
+		if (!to.isAdded()) { 
+			transaction.hide(from).add(R.id.rl_nearby_content, to).commit(); 
+		} else {
+			transaction.hide(from).show(to).commit();
+		}
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.btn_nearby_title_map:
+			switchContent(mNearByList, mNearByMap);
+			break;
+		case R.id.btn_nearby_title_list:
+			switchContent(mNearByMap, mNearByList);
+			break;
+		}
 	}
 
 }
