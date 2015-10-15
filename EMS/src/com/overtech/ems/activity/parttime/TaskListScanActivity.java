580f3b3dd89e8aec5b8d39da18b8c764e80cef08
Tony1213 <1973265001@qq.com>
2015-10-15 14:28:50 +0800

    添加Zxing包
    二维码扫描

---
+++ EMS/src/com/overtech/ems/activity/parttime/TaskListScanActivity.java
@@ -0,0 +1,11 @@
+package com.overtech.ems.activity.parttime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskListScanActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
