f17a06388eaf25b5e30a48980cfb31399e75c08e
Tony1213 <1973265001@qq.com>
2015-10-19 13:58:43 +0800

    添加”我的账单“页面代码
    兼职模块，我的账单

---
+++ EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAccountListActivity.java
@@ -0,0 +1,11 @@
+package com.overtech.ems.activity.parttime.personal;
+
+import android.os.Bundle;
+import com.overtech.ems.activity.BaseActivity;
+
+public class PersonalAccountListActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
