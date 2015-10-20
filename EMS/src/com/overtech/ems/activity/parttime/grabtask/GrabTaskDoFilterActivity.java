d98a52a04895846ba366efdbbf0d71d48ab4628a
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-10-20 14:41:18 +0800

    筛选
    区域和时间切换

--- EMS/src/com/overtech/ems/activity/parttime/grabtask/GrabTaskDoFilterActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/grabtask/GrabTaskDoFilterActivity.java
@@ -1,25 +1,30 @@
 package com.overtech.ems.activity.parttime.grabtask;
 
-import com.overtech.ems.R;
-import com.overtech.ems.activity.adapter.GridViewAdapter;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GrabTaskDoFilterActivity extends Activity {
+import com.overtech.ems.R;
+import com.overtech.ems.activity.adapter.GridViewAdapter;
+
+public class GrabTaskDoFilterActivity extends Activity implements OnClickListener {
 
 	private ImageView mHeadBack;
 	private TextView mHeadContent;
 	private TextView mHeadContentRight;
 	private GridView gridView;
 	private GridViewAdapter adapter;
+	private GridViewAdapter adapter2;
+	private Button button1;
+	private Button button2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,20 @@ public class GrabTaskDoFilterActivity extends Activity {
 		setContentView(R.layout.activity_grab_task_filter);
 		findViewById();
 		init();
+		flushContent();
+	}
+
+	private void flushContent() {
+		button1.setOnClickListener(this);
+		button2.setOnClickListener(this);
 	}
 
 	private void findViewById() {
 		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
 		mHeadBack = (ImageView) findViewById(R.id.iv_headBack);
 		mHeadContentRight = (TextView) findViewById(R.id.tv_headTitleRight);
+		button1=(Button) findViewById(R.id.button1);
+		button2=(Button) findViewById(R.id.button2);
 		gridView = (GridView) findViewById(R.id.gridView1);
 	}
 
@@ -50,12 +63,51 @@ public class GrabTaskDoFilterActivity extends Activity {
 		adapter = new GridViewAdapter(image, getApplicationContext());
 		gridView.setAdapter(adapter);
 		gridView.setOnItemClickListener(new OnItemClickListener() {
-
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1,
 					int position, long arg3) {
 				adapter.chiceState(position);
 			}
 		});
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button1:
+			if(adapter!=null){
+				adapter.notifyDataSetChanged();
+			}
+			gridView.setAdapter(adapter);
+			gridView.setOnItemClickListener(new OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int position, long arg3) {
+					adapter.chiceState(position);
+				}
+			});
+			break;
+		case R.id.button2:
+			int[] image2={R.drawable.one,R.drawable.two,R.drawable.three2five,R.drawable.five2seven,
+					R.drawable.seven,R.drawable.halfmonth,R.drawable.month};
+			if(adapter2!=null){
+				adapter2.notifyDataSetChanged();
+			}else{
+				adapter2=new GridViewAdapter(image2, getApplicationContext());
+			}
+			gridView.setAdapter(adapter2);
+			gridView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int position, long arg3) {
+					adapter2.chiceState(position);
+				}
+			});
+			break;
+		default:
+			break;
+		}
 	}
 }
