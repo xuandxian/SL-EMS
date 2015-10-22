8a318026251ffbf2f397ca37ba39eb8b70aca5ec
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-10-22 10:26:05 +0800

    手机号码更换完成页面添加自定义进度框

--- EMS/src/com/overtech/ems/activity/parttime/personal/ChangePhoneNoSuccessActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/personal/ChangePhoneNoSuccessActivity.java
@@ -10,11 +10,13 @@ import android.widget.TextView;
 
 import com.overtech.ems.R;
 import com.overtech.ems.activity.BaseActivity;
+import com.overtech.ems.widget.CustomProgressDialog;
 
 public class ChangePhoneNoSuccessActivity extends BaseActivity implements OnClickListener {
 	private TextView mHeadContent;
 	private ImageView mDoBack;
 	private Button mNextContent;
+	private CustomProgressDialog progressDialog = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -26,10 +28,11 @@ public class ChangePhoneNoSuccessActivity extends BaseActivity implements OnClic
 	}
 
 	private void showDialog() {
-		ProgressDialog mDialog=new ProgressDialog(this);
-		mDialog.setMessage("信息更新中...");
-		mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		mDialog.show();
+		if (progressDialog == null) {
+			progressDialog = CustomProgressDialog.createDialog(this);
+			progressDialog.setMessage("正在查询中...");
+		}
+		progressDialog.show();
 	}
 
 	private void initEvent() {
