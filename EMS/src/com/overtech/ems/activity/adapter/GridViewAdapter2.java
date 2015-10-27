acf14dd46d1946beb028f7aee40c3bb2ac7a7fc4
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-10-27 18:37:56 +0800

    电梯品牌优化

---
+++ EMS/src/com/overtech/ems/activity/adapter/GridViewAdapter2.java
@@ -0,0 +1,75 @@
+package com.overtech.ems.activity.adapter;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import com.overtech.ems.R;
+
+public class GridViewAdapter2 extends BaseAdapter {
+	
+	private String[] data;
+	private Context context;
+	private HashMap<Integer,Boolean> isSelected;
+	public GridViewAdapter2(String[] data,Context context) {
+		this.data=data;
+		this.context=context;
+		isSelected=new HashMap<Integer, Boolean>();
+	}
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return data.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		if(convertView==null){
+			convertView=LayoutInflater.from(context).inflate(R.layout.item_gridview_elevator_brand, null);
+		}
+		CheckBox box=(CheckBox) convertView.findViewById(R.id.cb_elevator);
+		box.setText(data[position]);
+		if(box.isChecked()){
+			isSelected.put(position, true);
+		}else{
+			if(isSelected.containsKey(position)){
+				isSelected.remove(position);
+			}
+		}
+		box.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked){
+					isSelected.put(position, isChecked);
+				}else{
+					isSelected.remove(position);
+				}
+			}
+		});
+		return convertView;
+	}
+	public HashMap getCheckBox(){
+		return isSelected;
+	}
+
+}
--- EMS/src/com/overtech/ems/activity/common/RegisterAddPersonEduAndWorkActivity.java
+++ EMS/src/com/overtech/ems/activity/common/RegisterAddPersonEduAndWorkActivity.java
@@ -1,21 +1,49 @@
 package com.overtech.ems.activity.common;
 
-import com.overtech.ems.R;
-import com.overtech.ems.activity.BaseActivity;
+import java.util.HashMap;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.overtech.ems.R;
+import com.overtech.ems.activity.BaseActivity;
+import com.overtech.ems.activity.adapter.GridViewAdapter2;
+import com.overtech.ems.widget.popwindow.DimPopupWindow;
 
 public class RegisterAddPersonEduAndWorkActivity extends BaseActivity {
 	private TextView mHeadContent;
 	private ImageView mHeadBack;
 	private Button mGoIdCard;
-
+	private Context mActivity;
+	private RelativeLayout mElevatorBrand;
+	private TextView mElevator;
+	private DimPopupWindow mPopupWindow;
+	private GridViewAdapter2 adapter;
+	private GridView mGridView;
+	private Button mConfirm;
+	private Button mCancle;
+	private HashMap<Integer,Boolean> isSelected;
+	private StringBuilder mCheckedMessage;
+	private String[] data={"日立","广日","上海三菱","日本三菱",
+			"通力","巨人通力","奥的斯","西子奥的斯",
+			"西奥","迅达","许昌西继","东芝","蒂森克虏伯",
+			"富士达","西尼","上海富士","华升富士达","浦东开灵",
+			"长江斯迈普","三荣","永大","现代","华特","爱登堡",
+			"新时达","崇友","德胜米高","阿尔法","上海房屋设备",
+			"席尔诺","森赫（原莱茵）","大连星玛","博林特","三洋",
+			"江南嘉捷","扬州三星","东南","康力","宏大","京都",
+			"曼隆","巨立","帝奥","德奥","霍普曼","怡达","快速",
+			"沃克斯","恒达富士","西屋","铃木","菱王"," 其他"};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,17 +60,71 @@ public class RegisterAddPersonEduAndWorkActivity extends BaseActivity {
 				startActivity(intent);
 			}
 		});
+		dealElevator();
+	}
+
+	private void dealElevator() {
+		if(adapter!=null){
+			isSelected=adapter.getCheckBox();
+			mCheckedMessage=new StringBuilder();
+			for(int i=0;i<data.length;i++){
+				if(isSelected.containsKey(i)){
+					mCheckedMessage.append(data[i]+"|");
+				}
+			}
+			mElevator.setText(mCheckedMessage.toString());
+		}
 	}
 
 	private void init() {
 		mHeadContent.setText("学历/工作信息");
 		mHeadBack.setVisibility(View.VISIBLE);
+		mElevatorBrand.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showPopupWindow();
+			}
+		});
+	}
+
+	protected void showPopupWindow() {
+		mPopupWindow=new DimPopupWindow(this);
+		View view=LayoutInflater.from(context).inflate(R.layout.register_gridview_elevator_brand, null);
+		mGridView=(GridView) view.findViewById(R.id.gridViewElevator);
+		mConfirm=(Button) view.findViewById(R.id.bt_elevator_confirm);
+		mCancle=(Button) view.findViewById(R.id.bt_elevator_cancle);
+		adapter=new GridViewAdapter2(data,mActivity);
+		mGridView.setAdapter(adapter);
+		mPopupWindow.setOutsideTouchable(false);
+		mPopupWindow.setContentView(view);
+		mPopupWindow.showAtLocation(mElevatorBrand, Gravity.RIGHT|Gravity.BOTTOM,0,0);
+		
+		mConfirm.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				dealElevator();
+				mPopupWindow.dismiss();
+			}
+		});
+		mCancle.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPopupWindow.dismiss();
+			}
+		});
 	}
 
 	private void findViewById() {
+		mActivity=RegisterAddPersonEduAndWorkActivity.this;
 		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
 		mHeadBack = (ImageView) findViewById(R.id.iv_headBack);
 		mGoIdCard = (Button) findViewById(R.id.btn_add_id_card);
+		mElevatorBrand=(RelativeLayout) findViewById(R.id.rl_register_add_info_4);
+		mElevator=(TextView) findViewById(R.id.tv_elevator_message);
 	}
+	
 
 }
