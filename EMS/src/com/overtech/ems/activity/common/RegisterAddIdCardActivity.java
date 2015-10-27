99830f6b5ff37cae419a8889d6ac9c2d20913412
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-10-27 11:51:58 +0800

    注册页面完善

--- EMS/src/com/overtech/ems/activity/common/RegisterAddIdCardActivity.java
+++ EMS/src/com/overtech/ems/activity/common/RegisterAddIdCardActivity.java
@@ -1,17 +1,21 @@
 package com.overtech.ems.activity.common;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.overtech.ems.R;
 
 public class RegisterAddIdCardActivity extends Activity {
 	private TextView mHeadContent;
 	private ImageView mHeadBack;
-
+	private Button mNext;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,10 +28,19 @@ public class RegisterAddIdCardActivity extends Activity {
 	private void init() {
 		mHeadContent.setText("身份证确认");
 		mHeadBack.setVisibility(View.VISIBLE);
+		mNext.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RegisterAddIdCardActivity.this,RegisterAddWorkCertificateActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	private void findViewById() {
 		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
 		mHeadBack = (ImageView) findViewById(R.id.iv_headBack);
+		mNext=(Button) findViewById(R.id.btn_register_next_work);
 	}
 }
--- EMS/src/com/overtech/ems/activity/common/RegisterAddWorkCertificateActivity.java
+++ EMS/src/com/overtech/ems/activity/common/RegisterAddWorkCertificateActivity.java
@@ -1,11 +1,47 @@
 package com.overtech.ems.activity.common;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.overtech.ems.R;
 
 public class RegisterAddWorkCertificateActivity extends Activity {
+	
+	private ImageView mDoBack;
+	private TextView mHeadTitle ;
+	private Button mNext;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_register_add_work_certificate);
+		findViewById();
+		init();
+	}
+
+	private void init() {
+		mHeadTitle.setText("上岗证确认");
+		mDoBack.setVisibility(View.VISIBLE);
+		mNext.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent=new Intent(RegisterAddWorkCertificateActivity.this,RegisterAddOtherCertificateActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	private void findViewById() {
+		mDoBack=(ImageView) findViewById(R.id.iv_headBack);
+		mHeadTitle=(TextView) findViewById(R.id.tv_headTitle);
+		mNext=(Button) findViewById(R.id.btn_register_next_work);
 	}
 }
