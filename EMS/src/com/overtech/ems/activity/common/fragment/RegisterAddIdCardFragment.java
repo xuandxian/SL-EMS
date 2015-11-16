a0006a0e69e7402275a0a4f90fc2be39b15415c1
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-11-16 10:28:02 +0800

    Revert "Revert "注册界面重新设计""
    This reverts commit f24396eb3df0415e953337b9adc51139f363af4f.

--- EMS/src/com/overtech/ems/activity/common/fragment/RegisterAddIdCardFragment.java
+++ EMS/src/com/overtech/ems/activity/common/fragment/RegisterAddIdCardFragment.java
@@ -1,8 +1,7 @@
-package com.overtech.ems.activity.common;
-
-import java.io.File;
+package com.overtech.ems.activity.common.fragment;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -15,23 +14,21 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.overtech.ems.R;
+import com.overtech.ems.activity.common.RegisterActivity;
 import com.overtech.ems.utils.ImageCacheUtils;
 import com.overtech.ems.utils.Utilities;
 import com.overtech.ems.widget.popwindow.DimPopupWindow;
 
-public class RegisterAddIdCardActivity extends Activity implements OnClickListener {
-	private TextView mHeadContent;
-	private ImageView mHeadBack;
-	private Button mNext;
+public class RegisterAddIdCardFragment extends Fragment implements OnClickListener {
+	private Context mContext;
+	private View view;
 	private ImageView mIdCardFront;
 	private ImageView mIdCardOpposite;
-	private Context mActivity;
 	private DimPopupWindow mPopupWindow;
 	private Button mCamera;
 	private Button mPhoto;
@@ -45,40 +42,37 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
      * 打开照相机的requestcode.
      */
     private static final int PHOTO_CAPTURE = 0x2;
-    /** 
+      
+	/**
+	 * 打开相册或者图册返回的图片的地址
+	 */
+	private Uri frontUri=null;
+	private Uri oppositeUri=null;
+	/** 
      * 图片的target大小. 
-     */  
-    private static String photoPath = "/sdcard/EMS/";
-	private static String photoName = photoPath + "IDCard.jpg";
-	Uri imageUri = Uri.fromFile(new File(Environment
-			.getExternalStorageDirectory(), "image.jpg"));
-	
+     */
     private static final int target = 400;  
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_register_add_id_card);
-		mActivity=this;
-		findViewById();
-		init();
+    @Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		mContext=activity;
 	}
-
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view=inflater.inflate(R.layout.fragment_register_add_id_card, null);
+		findViewById(view);
+		init();
+		return view;
+	}
 	private void init() {
-		mHeadContent.setText("身份证确认");
-		mHeadBack.setVisibility(View.VISIBLE);
-		mNext.setOnClickListener(this);
-		mHeadBack.setOnClickListener(this);
 		mIdCardFront.setOnClickListener(this);
 		mIdCardOpposite.setOnClickListener(this);
 	}
-
-	private void findViewById() {
-		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
-		mHeadBack = (ImageView) findViewById(R.id.iv_headBack);
-		mNext=(Button) findViewById(R.id.btn_register_next_work);
-		mIdCardFront=(ImageView) findViewById(R.id.iv_idcard_front);
-		mIdCardOpposite=(ImageView) findViewById(R.id.iv_idcard_opposite);
+	private void findViewById(View v) {
+		mIdCardFront=(ImageView)v. findViewById(R.id.iv_idcard_front);
+		mIdCardOpposite=(ImageView) v.findViewById(R.id.iv_idcard_opposite);
 	}
 	/**
 	 * 记录身份证点击的状态
@@ -86,15 +80,7 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
 	private int currentState;
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.iv_headBack:
-			finish();
-			break;
-		case R.id.btn_register_next_work:
-			Intent intent = new Intent(RegisterAddIdCardActivity.this,RegisterAddWorkCertificateActivity.class);
-			startActivity(intent);
-			break;
 		case R.id.iv_idcard_front:
 			currentState=0;
 			showPopupWindow(v);
@@ -104,33 +90,26 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
 			showPopupWindow(v);
 			break;
 		case R.id.item_popupwindows_camera:
-			Utilities.showToast("你点击了照相机", mActivity);
+			Utilities.showToast("你点击了照相机", mContext);
 			openCamera();
 			mPopupWindow.dismiss();
 			break;
 		case R.id.item_popupwindows_Photo:
-			Utilities.showToast("你点击了相册", mActivity);
+			Utilities.showToast("你点击了相册", mContext);
 			openPhotos();
 			mPopupWindow.dismiss();
 			break;
 		case R.id.item_popupwindows_cancel:
-			Utilities.showToast("你点击了取消", mActivity);
+			Utilities.showToast("你点击了取消", mContext);
 			mPopupWindow.dismiss();
 			break;
 		default:
 			break;
 		}
 	}
-
 	private void openCamera() {
 		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-		File file = new File(photoPath);
-		if (!file.exists()) { // 检查图片存放的文件夹是否存在
-			file.mkdir(); // 不存在的话 创建文件夹
-		}
-		File photo = new File(photoName);
-		imageUri = Uri.fromFile(photo);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); // 这样就将文件的存储方式和uri指定到了Camera应用中
+//		intent.putExtra(MediaStore.EXTRA_OUTPUT, frontUri); // 这样就将文件的存储方式和uri指定到了Camera应用中
 		startActivityForResult(intent, PHOTO_CAPTURE);
 	}
 
@@ -138,11 +117,11 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
 		Intent intent = new Intent(Intent.ACTION_PICK, null);  
         intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,  
                 "image/*");  
-  
         startActivityForResult(intent, OPEN_PHOTO_REQUESTCODE);
 	}
+	
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		String sdStatus = Environment.getExternalStorageState();
 		if(!sdStatus.equals(Environment.MEDIA_MOUNTED)){
 			Log.i("内存卡错误", "请检查您的内存卡");
@@ -150,28 +129,36 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
 		}
 		switch (requestCode) {
 		case OPEN_PHOTO_REQUESTCODE:
-			if(resultCode == RESULT_OK){  
+			if(resultCode == Activity.RESULT_OK){  
                 //如果用这个方法，Options为null时候，就是默认decode会出现oom哦.  
                 //Bitmap bm = ImageCacheUtil.decode(null, null,   
                 //      ImageCacheDemoActivity.this, data.getData(), null);  
                   
                 //这里调用这个方法就不会oom.屌丝们就用这个方法吧.  
                 Bitmap bm = ImageCacheUtils.getResizedBitmap(null, null,   
-                        mActivity, data.getData(), target, false);  
+                        mContext, data.getData(), target, false);  
                 if(currentState==0){
                 	mIdCardFront.setImageBitmap(bm);  
+                	//记录打开相册获取的图片的uri，赋给
+                	frontUri=data.getData();
                 }else if(currentState==1){
                 	mIdCardOpposite.setImageBitmap(bm);
+                	oppositeUri=data.getData();
                 }
             }
 			break;
 		case PHOTO_CAPTURE:
-			Bitmap pic=ImageCacheUtils.getBitmap(photoName);
-			if(currentState==0){
-            	mIdCardFront.setImageBitmap(pic);  
-            }else if(currentState==1){
-            	mIdCardOpposite.setImageBitmap(pic);
-            }
+			if(resultCode == Activity.RESULT_OK){
+				Bitmap pic = ImageCacheUtils.getResizedBitmap(null, null,   
+	                    mContext, data.getData(), target, false);
+				if(currentState==0){
+					frontUri=data.getData();
+	            	mIdCardFront.setImageBitmap(pic);  
+	            }else if(currentState==1){
+	            	mIdCardOpposite.setImageBitmap(pic);
+	            	oppositeUri=data.getData();
+	            }
+			}
 			break;
 
 		default:
@@ -180,20 +167,27 @@ public class RegisterAddIdCardActivity extends Activity implements OnClickListen
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 	private void showPopupWindow(View v) {
-		mPopupWindow=new DimPopupWindow(mActivity);
-		View contentView=LayoutInflater.from(mActivity).inflate(R.layout.layout_dim_pop_add_idcard, null);
-		findViewById(contentView);
+		mPopupWindow=new DimPopupWindow(mContext);
+		View contentView=LayoutInflater.from(mContext).inflate(R.layout.layout_dim_pop_add_idcard, null);
+		initView(contentView);
 		mPopupWindow.setContentView(contentView);
 		mPopupWindow.setInAnimation(R.anim.register_add_idcard_in);
-		mPopupWindow.showAtLocation(getWindow().getDecorView().getRootView(), Gravity.BOTTOM, 0, 0);
+		mPopupWindow.showAtLocation(((Activity) mContext).getWindow().getDecorView().getRootView(), Gravity.BOTTOM, 0, 0);
 	}
-
-	private void findViewById(View contentView) {
+	private void initView(View contentView) {
 		mCamera=(Button) contentView.findViewById(R.id.item_popupwindows_camera);
 		mPhoto=(Button) contentView.findViewById(R.id.item_popupwindows_Photo);
 		mCancle=(Button) contentView.findViewById(R.id.item_popupwindows_cancel);
 		mCamera.setOnClickListener(this);
 		mPhoto.setOnClickListener(this);
 		mCancle.setOnClickListener(this);
+	}
+	public boolean isAllNotNull(){
+		if(frontUri!=null&&oppositeUri!=null){
+			return true;
+		}else{
+			Utilities.showToast("您还没有选择证件", mContext);
+			return false;
+		}
 	}
 }
