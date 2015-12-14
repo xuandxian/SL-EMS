a7dac99a14efd71b79553b9322dccf8996d6f33e
zAndroidDeveloper <zAndroidDeveloper@163.com>
2015-12-14 11:41:49 +0800

    创建电梯详情实体类

---
+++ EMS/src/com/overtech/ems/entity/parttime/ElevatorInfo.java
@@ -0,0 +1,435 @@
+package com.overtech.ems.entity.parttime;
+
+public class ElevatorInfo {
+	/**
+	 * 项目名称
+	 */
+	private String projectName;
+	/**
+	 * 电梯品牌
+	 */
+	private String elevatorBrand;
+	/**
+	 * 电梯型号
+	 */
+	private String elevatorModel;
+	/**
+	 * 电梯编号
+	 */
+	private String elevatorNo;
+	/**
+	 * 梯号
+	 */
+	private String elevatorAliase;
+	/**
+	 * 物业公司
+	 */
+	private String tenementCompany;
+	/**
+	 * 物业联系人
+	 */
+	private String tenementPerson;
+	/**
+	 * 物业联系电话
+	 */
+	private String tenementTel;
+	/**
+	 * 维保公司
+	 */
+	private String maintenanceCompany;
+	/**
+	 * 维保站点
+	 */
+	private String maintenanceSite;
+	/**
+	 * 维保站点电话
+	 */
+	private String maintenanceSiteTel;
+	/**
+	 * 二维码信息
+	 */
+	private String tdCode;
+	/**
+	 * 额定载重
+	 */
+	private String loadCapacity;
+	/**
+	 * 额定速度
+	 */
+	private String nominalSpeed;
+	/**
+	 * 层/站/门
+	 */
+	private String storeyPlatformDoor;
+	/**
+	 * 提升高度
+	 */
+	private String elevatorHigher;
+	/**
+	 * 顶层高度
+	 */
+	private String topHeight;
+	/**
+	 * 底坑深度
+	 */
+	private String bottomDepth;
+	/**
+	 * 开门方式
+	 */
+	private String doorWay;
+	/**
+	 * 开门尺寸
+	 */
+	private String doorSize;
+	/**
+	 * 轿厢尺寸
+	 */
+	private String boxSize;
+	/**
+	 * 制造单位
+	 */
+	private String makeUnit;
+	/**
+	 * 电梯类型
+	 */
+	private String elevatorType;
+	/**
+	 * 注册代码
+	 */
+	private String registerCode;
+	/**
+	 * 登记机关
+	 */
+	private String registerOffice;
+	/**
+	 * 检验单位
+	 */
+	private String checkUnit;
+	/**
+	 * 监测系统设备号
+	 */
+	private String monitorSysCode;
+	/**
+	 * 监测系统设备安装日期
+	 */
+	private String monitorSysDate;
+	/**
+	 * 上家维保公司
+	 */
+	private String lastMaintenanceCompany;
+	/**
+	 * 出厂编码
+	 */
+	private String factoryCode;
+	/**
+	 * 出厂日期
+	 */
+	private String factoryDate;
+	/**
+	 * 验收合格日期
+	 */
+	private String checkDate;
+	/**
+	 * 销售合同编号
+	 */
+	private String saleContractNo;
+	/**
+	 * 保养类型
+	 */
+	private String maintenanceType;
+	/**
+	 * 安装公司
+	 */
+	private String intallCompany;
+	/**
+	 * 安装合同编号
+	 */
+	private String intallContractNo;
+	/**
+	 * 设备地址
+	 */
+	private String deviceAddress;
+	/**
+	 * 年检日期
+	 */
+	private String annualInspectionDate;
+	/**
+	 * 上次保养日期
+	 */
+	private String lastMaintenanceDate;
+	/**
+	 * 经度
+	 */
+	private String longitude;
+	/**
+	 * 纬度
+	 */
+	private String latitude;
+	/**
+	 * 维保合同编号
+	 */
+	private String maintenanceContractNo;
+	/**
+	 * 备注
+	 */
+	private String remark;
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getElevatorBrand() {
+		return elevatorBrand;
+	}
+	public void setElevatorBrand(String elevatorBrand) {
+		this.elevatorBrand = elevatorBrand;
+	}
+	public String getElevatorModel() {
+		return elevatorModel;
+	}
+	public void setElevatorModel(String elevatorModel) {
+		this.elevatorModel = elevatorModel;
+	}
+	public String getElevatorNo() {
+		return elevatorNo;
+	}
+	public void setElevatorNo(String elevatorNo) {
+		this.elevatorNo = elevatorNo;
+	}
+	public String getElevatorAliase() {
+		return elevatorAliase;
+	}
+	public void setElevatorAliase(String elevatorAliase) {
+		this.elevatorAliase = elevatorAliase;
+	}
+	public String getTenementCompany() {
+		return tenementCompany;
+	}
+	public void setTenementCompany(String tenementCompany) {
+		this.tenementCompany = tenementCompany;
+	}
+	public String getTenementPerson() {
+		return tenementPerson;
+	}
+	public void setTenementPerson(String tenementPerson) {
+		this.tenementPerson = tenementPerson;
+	}
+	public String getTenementTel() {
+		return tenementTel;
+	}
+	public void setTenementTel(String tenementTel) {
+		this.tenementTel = tenementTel;
+	}
+	public String getMaintenanceCompany() {
+		return maintenanceCompany;
+	}
+	public void setMaintenanceCompany(String maintenanceCompany) {
+		this.maintenanceCompany = maintenanceCompany;
+	}
+	public String getMaintenanceSite() {
+		return maintenanceSite;
+	}
+	public void setMaintenanceSite(String maintenanceSite) {
+		this.maintenanceSite = maintenanceSite;
+	}
+	public String getMaintenanceSiteTel() {
+		return maintenanceSiteTel;
+	}
+	public void setMaintenanceSiteTel(String maintenanceSiteTel) {
+		this.maintenanceSiteTel = maintenanceSiteTel;
+	}
+	public String getTdCode() {
+		return tdCode;
+	}
+	public void setTdCode(String tdCode) {
+		this.tdCode = tdCode;
+	}
+	public String getLoadCapacity() {
+		return loadCapacity;
+	}
+	public void setLoadCapacity(String loadCapacity) {
+		this.loadCapacity = loadCapacity;
+	}
+	public String getNominalSpeed() {
+		return nominalSpeed;
+	}
+	public void setNominalSpeed(String nominalSpeed) {
+		this.nominalSpeed = nominalSpeed;
+	}
+	public String getStoreyPlatformDoor() {
+		return storeyPlatformDoor;
+	}
+	public void setStoreyPlatformDoor(String storeyPlatformDoor) {
+		this.storeyPlatformDoor = storeyPlatformDoor;
+	}
+	public String getElevatorHigher() {
+		return elevatorHigher;
+	}
+	public void setElevatorHigher(String elevatorHigher) {
+		this.elevatorHigher = elevatorHigher;
+	}
+	public String getTopHeight() {
+		return topHeight;
+	}
+	public void setTopHeight(String topHeight) {
+		this.topHeight = topHeight;
+	}
+	public String getBottomDepth() {
+		return bottomDepth;
+	}
+	public void setBottomDepth(String bottomDepth) {
+		this.bottomDepth = bottomDepth;
+	}
+	public String getDoorWay() {
+		return doorWay;
+	}
+	public void setDoorWay(String doorWay) {
+		this.doorWay = doorWay;
+	}
+	public String getDoorSize() {
+		return doorSize;
+	}
+	public void setDoorSize(String doorSize) {
+		this.doorSize = doorSize;
+	}
+	public String getBoxSize() {
+		return boxSize;
+	}
+	public void setBoxSize(String boxSize) {
+		this.boxSize = boxSize;
+	}
+	public String getMakeUnit() {
+		return makeUnit;
+	}
+	public void setMakeUnit(String makeUnit) {
+		this.makeUnit = makeUnit;
+	}
+	public String getElevatorType() {
+		return elevatorType;
+	}
+	public void setElevatorType(String elevatorType) {
+		this.elevatorType = elevatorType;
+	}
+	public String getRegisterCode() {
+		return registerCode;
+	}
+	public void setRegisterCode(String registerCode) {
+		this.registerCode = registerCode;
+	}
+	public String getRegisterOffice() {
+		return registerOffice;
+	}
+	public void setRegisterOffice(String registerOffice) {
+		this.registerOffice = registerOffice;
+	}
+	public String getCheckUnit() {
+		return checkUnit;
+	}
+	public void setCheckUnit(String checkUnit) {
+		this.checkUnit = checkUnit;
+	}
+	public String getMonitorSysCode() {
+		return monitorSysCode;
+	}
+	public void setMonitorSysCode(String monitorSysCode) {
+		this.monitorSysCode = monitorSysCode;
+	}
+	public String getMonitorSysDate() {
+		return monitorSysDate;
+	}
+	public void setMonitorSysDate(String monitorSysDate) {
+		this.monitorSysDate = monitorSysDate;
+	}
+	public String getLastMaintenanceCompany() {
+		return lastMaintenanceCompany;
+	}
+	public void setLastMaintenanceCompany(String lastMaintenanceCompany) {
+		this.lastMaintenanceCompany = lastMaintenanceCompany;
+	}
+	public String getFactoryCode() {
+		return factoryCode;
+	}
+	public void setFactoryCode(String factoryCode) {
+		this.factoryCode = factoryCode;
+	}
+	public String getFactoryDate() {
+		return factoryDate;
+	}
+	public void setFactoryDate(String factoryDate) {
+		this.factoryDate = factoryDate;
+	}
+	public String getCheckDate() {
+		return checkDate;
+	}
+	public void setCheckDate(String checkDate) {
+		this.checkDate = checkDate;
+	}
+	public String getSaleContractNo() {
+		return saleContractNo;
+	}
+	public void setSaleContractNo(String saleContractNo) {
+		this.saleContractNo = saleContractNo;
+	}
+	public String getMaintenanceType() {
+		return maintenanceType;
+	}
+	public void setMaintenanceType(String maintenanceType) {
+		this.maintenanceType = maintenanceType;
+	}
+	public String getIntallCompany() {
+		return intallCompany;
+	}
+	public void setIntallCompany(String intallCompany) {
+		this.intallCompany = intallCompany;
+	}
+	public String getIntallContractNo() {
+		return intallContractNo;
+	}
+	public void setIntallContractNo(String intallContractNo) {
+		this.intallContractNo = intallContractNo;
+	}
+	public String getDeviceAddress() {
+		return deviceAddress;
+	}
+	public void setDeviceAddress(String deviceAddress) {
+		this.deviceAddress = deviceAddress;
+	}
+	public String getAnnualInspectionDate() {
+		return annualInspectionDate;
+	}
+	public void setAnnualInspectionDate(String annualInspectionDate) {
+		this.annualInspectionDate = annualInspectionDate;
+	}
+	public String getLastMaintenanceDate() {
+		return lastMaintenanceDate;
+	}
+	public void setLastMaintenanceDate(String lastMaintenanceDate) {
+		this.lastMaintenanceDate = lastMaintenanceDate;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getMaintenanceContractNo() {
+		return maintenanceContractNo;
+	}
+	public void setMaintenanceContractNo(String maintenanceContractNo) {
+		this.maintenanceContractNo = maintenanceContractNo;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+}
