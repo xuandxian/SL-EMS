8c2150d634aa4783ccb36d594fdd405c46ba659d
Tony1213 <1973265001@qq.com>
2015-12-15 11:04:45 +0800

    自定义webview
    webview

--- EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAboutCompanyActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAboutCompanyActivity.java
@@ -9,9 +9,8 @@ import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.overtech.ems.R;
 import com.overtech.ems.activity.BaseActivity;
+import com.overtech.ems.R;
 
 public class PersonalAboutCompanyActivity extends BaseActivity {
 	private TextView mHeadContent;
@@ -27,30 +26,41 @@ public class PersonalAboutCompanyActivity extends BaseActivity {
 		initView();
 		init();
 	}
+
+	private void initView() {
+		mHeadContent=(TextView) findViewById(R.id.tv_headTitle);
+		mDoBack=(ImageView) findViewById(R.id.iv_headBack);
+		mWebView=(WebView) findViewById(R.id.wv_announcement);
+		mProgressBar=(ProgressBar) findViewById(R.id.progressBar);
+		mSetting=mWebView.getSettings();
+	}
+
 	private void init() {
 		mHeadContent.setText("公司主页");
 		mDoBack.setVisibility(View.VISIBLE);
-		mWebView.setWebChromeClient(new WebChromeClient(){
-			
+		mWebView.setWebChromeClient(new WebChromeClient() {
+
 			@Override
 			public void onProgressChanged(WebView view, int newProgress) {
 				// TODO Auto-generated method stub
 				super.onProgressChanged(view, newProgress);
-				if(newProgress==100){
+				if (newProgress == 100) {
 					mProgressBar.setVisibility(View.GONE);
-				}else{
-					if(View.VISIBLE==mProgressBar.getVisibility()){
+				} else {
+					if (View.VISIBLE == mProgressBar.getVisibility()) {
 						mProgressBar.setVisibility(View.VISIBLE);
 					}
 					mProgressBar.setProgress(newProgress);
 				}
 			}
-			
+
 		});
 		mWebView.loadUrl("http://www.relevator.cc");
 		mSetting.setUseWideViewPort(true);
 		mSetting.setBuiltInZoomControls(true);
 		mSetting.setSupportZoom(true);
+		mSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+		mSetting.setAppCacheEnabled(true);
 		mWebView.setWebViewClient(new WebViewClient(){
 			@Override
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -58,12 +68,5 @@ public class PersonalAboutCompanyActivity extends BaseActivity {
 				return true;
 			}
 		});
-	}
-	private void initView() {
-		mHeadContent=(TextView) findViewById(R.id.tv_headTitle);
-		mDoBack=(ImageView) findViewById(R.id.iv_headBack);
-		mWebView=(WebView) findViewById(R.id.wv_announcement);
-		mProgressBar=(ProgressBar) findViewById(R.id.progressBar);
-		mSetting=mWebView.getSettings();
 	}
 }
--- EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAppDeveloperActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAppDeveloperActivity.java
@@ -1,68 +1,56 @@
 package com.overtech.ems.activity.parttime.personal;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.DownloadListener;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.overtech.ems.R;
 import com.overtech.ems.activity.BaseActivity;
+import com.overtech.ems.widget.webview.ProgressWebView;
+import android.net.Uri;
 
 public class PersonalAppDeveloperActivity extends BaseActivity {
-	private TextView mHead;
-	private ImageView mDoBack;
-	private WebView mDevelopers;
-	private WebSettings mSetting;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_personal_app_developers);
-		initView();
-		initData();
-	}
+    private TextView mHead;
+    private ImageView mDoBack;
+    private ProgressWebView webview;
 
-	private void initData() {
-		mHead.setText("APP开发团队");
-		mDoBack.setVisibility(View.VISIBLE);
-		mDoBack.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
-		mDevelopers.loadUrl("http://www.overtech.com.cn");
-		mSetting.setJavaScriptEnabled(true);
-		mDevelopers.setWebViewClient(new MyWebViewClient());
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_app_developers);
+        initView();
+        initData();
+    }
 
-	private void initView() {
-		mHead=(TextView) findViewById(R.id.tv_headTitle);
-		mDoBack=(ImageView) findViewById(R.id.iv_headBack);
-		mDevelopers=(WebView) findViewById(R.id.wv_developers);
-		mSetting=mDevelopers.getSettings();
-	}
-	private class MyWebViewClient extends WebViewClient{
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			// TODO Auto-generated method stub
-			view.loadUrl(url);
-			return true;
-		}
-	}
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		if(keyCode==KeyEvent.KEYCODE_BACK&&mDevelopers.canGoBack()){
-			mDevelopers.goBack();
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+    private void initView() {
+        mHead = (TextView) findViewById(R.id.tv_headTitle);
+        mDoBack = (ImageView) findViewById(R.id.iv_headBack);
+        webview = (ProgressWebView) findViewById(R.id.webview_developer);
+    }
+
+    private void initData() {
+        mHead.setText("APP开发团队");
+        mDoBack.setVisibility(View.VISIBLE);
+        mDoBack.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        String url="http://m.ly.com/?RefId=4140683";
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                if (url != null && url.startsWith("http://"))
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
+
+        webview.loadUrl(url);
+    }
 }
