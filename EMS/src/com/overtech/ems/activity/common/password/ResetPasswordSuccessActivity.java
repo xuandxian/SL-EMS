fb20f4d1290a107f572503fa3e0719d351d22cb3
zAndroidDeveloper <zAndroidDeveloper@163.com>
2016-02-17 12:44:39 +0800

    优化密码修改成功

--- EMS/src/com/overtech/ems/activity/common/password/ResetPasswordSuccessActivity.java
+++ EMS/src/com/overtech/ems/activity/common/password/ResetPasswordSuccessActivity.java
@@ -1,9 +1,5 @@
 package com.overtech.ems.activity.common.password;
 
-import com.overtech.ems.R;
-import com.overtech.ems.activity.common.LoginActivity;
-import com.overtech.ems.widget.CustomProgressDialog;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,15 +8,18 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ResetPasswordSuccessActivity extends Activity {
+import com.overtech.ems.R;
+import com.overtech.ems.activity.BaseActivity;
+import com.overtech.ems.activity.common.LoginActivity;
+import com.overtech.ems.widget.CustomProgressDialog;
+
+public class ResetPasswordSuccessActivity extends BaseActivity {
 	private TextView mHeadContent;
 	private ImageView mHeadBack;
-	private CustomProgressDialog progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_reset_password_success);
 		findViewById();
 		init();
@@ -34,14 +33,14 @@ public class ResetPasswordSuccessActivity extends Activity {
 	private void init() {
 		mHeadContent.setText("密码重置");
 		mHeadBack.setVisibility(View.VISIBLE);
-		progressDialog = CustomProgressDialog.createDialog(this);
+		progressDialog.setMessage("正在重新登录...");
+		progressDialog.show();
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				progressDialog.setMessage("正在重新登录...");
-				progressDialog.show();
 				Intent intent=new Intent(ResetPasswordSuccessActivity.this,LoginActivity.class);
 				startActivity(intent);
+				progressDialog.dismiss();
 				finish();
 			}
 		}, 2000);
