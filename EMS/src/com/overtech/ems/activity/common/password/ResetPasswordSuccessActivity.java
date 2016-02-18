cf196ea3e81ae1d1053e87b11bdcb3d368e1b47b
Tony1213 <1973265001@qq.com>
2016-02-18 14:43:51 +0800

    修改重置密码提示

--- EMS/src/com/overtech/ems/activity/common/password/ResetPasswordSuccessActivity.java
+++ EMS/src/com/overtech/ems/activity/common/password/ResetPasswordSuccessActivity.java
@@ -3,13 +3,13 @@ package com.overtech.ems.activity.common.password;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.overtech.ems.R;
 import com.overtech.ems.activity.BaseActivity;
 import com.overtech.ems.activity.common.LoginActivity;
+import com.overtech.ems.utils.Utilities;
 
 public class ResetPasswordSuccessActivity extends BaseActivity {
 	private TextView mHeadContent;
@@ -31,12 +31,10 @@ public class ResetPasswordSuccessActivity extends BaseActivity {
 	private void init() {
 		mHeadContent.setText("密码重置");
 		mHeadBack.setVisibility(View.VISIBLE);
+		Utilities.showToast("2秒后跳转到登录", context);
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				progressDialog.setMessage("正在重新登录...");
-				progressDialog.show();
-				SystemClock.sleep(1000);
 				Intent intent=new Intent(ResetPasswordSuccessActivity.this,LoginActivity.class);
 				startActivity(intent);
 				progressDialog.dismiss();
