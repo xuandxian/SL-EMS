50ae8be41a8dcf8229e2a9b7d7f94c5ad5f52ae7
zAndroidDeveloper <zAndroidDeveloper@163.com>
2016-03-11 09:52:28 +0800

    公告实现记录点击记录，显示点击过的颜色变化

--- EMS/src/com/overtech/ems/activity/adapter/PersonalAnnounceAdapter.java
+++ EMS/src/com/overtech/ems/activity/adapter/PersonalAnnounceAdapter.java
@@ -1,8 +1,12 @@
 package com.overtech.ems.activity.adapter;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.TreeSet;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +14,31 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.overtech.ems.R;
+import com.overtech.ems.activity.MyApplication;
 import com.overtech.ems.entity.parttime.Announcement;
 
 public class PersonalAnnounceAdapter extends BaseAdapter {
-	
+
 	private Context ctx;
 	private List<Announcement> list;
-	public  PersonalAnnounceAdapter(Context context,List<Announcement> list){
-		this.ctx=context;
-		this.list=list;
+	private HashSet<String> announceItemPosition;
+	private final String ANNOUNCEITEMPOSITION="announce_item_position";
+
+	public PersonalAnnounceAdapter(Context context, List<Announcement> list,HashSet<String> set) {
+		this.ctx = context;
+		this.list = list;
+		announceItemPosition = set;
 	}
+
+	/**
+	 * 当listview条目点击改变时，将刷新后的set集合让adapter使用
+	 * 
+	 * @param set
+	 */
+	public void setHashSet(HashSet<String> set) {
+		announceItemPosition = set;
+	}
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -40,29 +59,51 @@ public class PersonalAnnounceAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		Announcement data=list.get(position);
-		ViewHolder vh=null;
-		if(convertView==null){
-			convertView=LayoutInflater.from(ctx).inflate(R.layout.item_list_personal_announcement, null);
-			vh=new ViewHolder(convertView);
+		Announcement data = list.get(position);
+		ViewHolder vh = null;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(ctx).inflate(
+					R.layout.item_list_personal_announcement, null);
+			vh = new ViewHolder(convertView);
 			convertView.setTag(vh);
-		}else{
-			vh=(ViewHolder) convertView.getTag();
+		} else {
+			vh = (ViewHolder) convertView.getTag();
+		}
+		
+		if (announceItemPosition.contains(String.valueOf(position))) {
+			vh.tvAnnouncementSummary.setTextColor(ctx.getResources().getColor(
+					R.color.announce_after));
+			vh.tvAnnouncementTitle.setTextColor(ctx.getResources().getColor(
+					R.color.announce_after));
+			vh.tvAnnouncementTime.setTextColor(ctx.getResources().getColor(
+					R.color.announce_after));
+		} else {
+			vh.tvAnnouncementSummary.setTextColor(ctx.getResources().getColor(
+					R.color.announce_before));
+			vh.tvAnnouncementTitle.setTextColor(ctx.getResources().getColor(
+					R.color.announce_before));
+			vh.tvAnnouncementTime.setTextColor(ctx.getResources().getColor(
+					R.color.announce_before));
 		}
 		vh.tvAnnouncementSummary.setText(data.getSummary());
 		vh.tvAnnouncementTitle.setText(data.getTheme());
 		vh.tvAnnouncementTime.setText(data.getReleaseDate());
-		
+
 		return convertView;
 	}
-	private class ViewHolder{
+
+	private class ViewHolder {
 		TextView tvAnnouncementTitle;
 		TextView tvAnnouncementSummary;
 		TextView tvAnnouncementTime;
-		public ViewHolder(View view){
-			tvAnnouncementTitle=(TextView) view.findViewById(R.id.tv_announcement_title);
-			tvAnnouncementSummary=(TextView) view.findViewById(R.id.tv_announcement_abstract);
-			tvAnnouncementTime=(TextView) view.findViewById(R.id.tv_announcement_time);
+
+		public ViewHolder(View view) {
+			tvAnnouncementTitle = (TextView) view
+					.findViewById(R.id.tv_announcement_title);
+			tvAnnouncementSummary = (TextView) view
+					.findViewById(R.id.tv_announcement_abstract);
+			tvAnnouncementTime = (TextView) view
+					.findViewById(R.id.tv_announcement_time);
 		}
 	}
 }
--- EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAnnouncementActivity.java
+++ EMS/src/com/overtech/ems/activity/parttime/personal/PersonalAnnouncementActivity.java
@@ -1,9 +1,14 @@
 package com.overtech.ems.activity.parttime.personal;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import android.content.Intent;
+import android.content.ClipData.Item;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -30,36 +35,61 @@ import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
 /**
  * 公告栏
  * 
  */
 public class PersonalAnnouncementActivity extends BaseActivity {
 	private ImageView mDoBack;
-	private	TextView mHeadContent;
+	private TextView mHeadContent;
 	private ListView mAnnouncement;
 	private PersonalAnnounceAdapter adapter;
 	private List<Announcement> list;
-	private Handler handler=new Handler(){
+	private int announceSize;
+	private HashSet<String> announceItemPosition;
+	private final String ANNOUNCEITEMPOSITION="announce_item_position";
+	private final String ANNOUNCE_SIZE="announces_size";
+	private Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
 			case StatusCode.ANNOUNCEMENT_SUCCESS:
-				String json= (String) msg.obj;
-				Gson gson=new Gson();
-				AnnouncementBean bean=gson.fromJson(json, AnnouncementBean.class);
-				list=bean.getModel();
-				if(null==list||list.size()==0){
+				String json = (String) msg.obj;
+				Gson gson = new Gson();
+				AnnouncementBean bean = gson.fromJson(json,
+						AnnouncementBean.class);
+				list = bean.getModel();
+				if (null == list || list.size() == 0) {
 					Utilities.showToast("无数据", activity);
-				}else{
-					adapter=new PersonalAnnounceAdapter(context, list);
+				} else {
+					int newItems = list.size() - announceSize;//计算最新的数据集合的大小与之前的数据集合大小
+					if (newItems > 0) {
+						HashSet<String> tempSet = new HashSet<String>();//初始化一个tempSet,用于保存更新后的数据的已经点击的公告条目的位置
+						Iterator<String> iterator = announceItemPosition
+								.iterator();
+						while (iterator.hasNext()) {
+							int a = Integer.parseInt(iterator.next())
+									+ newItems;//如果原集合中有数据，则将保存的角标全部加上新的计算的差值
+							tempSet.add(String.valueOf(a));
+						}
+						announceItemPosition = tempSet;//使用最新的集合
+						mSharedPreferences
+								.edit()
+								.putStringSet(ANNOUNCEITEMPOSITION,
+										announceItemPosition).commit();//将集合保存到配置文件中
+						mSharedPreferences.edit()
+								.putInt(ANNOUNCE_SIZE, list.size()).commit();//将大小保存
+					}
+					adapter = new PersonalAnnounceAdapter(context, list,
+							announceItemPosition);
 					mAnnouncement.setAdapter(adapter);
 				}
 				break;
 			case StatusCode.RESPONSE_SERVER_EXCEPTION:
-				Utilities.showToast((String)msg.obj, context);
+				Utilities.showToast((String) msg.obj, context);
 				break;
 			case StatusCode.RESPONSE_NET_FAILED:
-				Utilities.showToast((String)msg.obj, context);
+				Utilities.showToast((String) msg.obj, context);
 				break;
 
 			default:
@@ -68,6 +98,7 @@ public class PersonalAnnouncementActivity extends BaseActivity {
 			stopProgressDialog();
 		};
 	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -76,11 +107,24 @@ public class PersonalAnnouncementActivity extends BaseActivity {
 		initData();
 	}
 
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.e("onResume", "onresume");
+		announceSize = mSharedPreferences.getInt(ANNOUNCE_SIZE, 0);// 获取保存的公告条目的长度
+		announceItemPosition = (HashSet<String>) mSharedPreferences
+				.getStringSet(ANNOUNCEITEMPOSITION, new HashSet<String>());// 获取已经点击过的公告条目的position集合
+		if (adapter != null) {
+			adapter.setHashSet(announceItemPosition);
+			adapter.notifyDataSetChanged();
+		}
+	}
 
 	private void initView() {
-		mHeadContent=(TextView) findViewById(R.id.tv_headTitle);
-		mDoBack=(ImageView) findViewById(R.id.iv_headBack);
-		mAnnouncement=(ListView)findViewById(R.id.lv_announcement);
+		mHeadContent = (TextView) findViewById(R.id.tv_headTitle);
+		mDoBack = (ImageView) findViewById(R.id.iv_headBack);
+		mAnnouncement = (ListView) findViewById(R.id.lv_announcement);
 		mHeadContent.setText("公告");
 		mDoBack.setVisibility(View.VISIBLE);
 		mDoBack.setOnClickListener(new OnClickListener() {
@@ -94,40 +138,56 @@ public class PersonalAnnouncementActivity extends BaseActivity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				Announcement data=(Announcement) parent.getAdapter().getItem(position);
-				Bundle bundle=new Bundle();
+				announceItemPosition.add(String.valueOf(position));//将点击的位置保存到集合中
+				mSharedPreferences.edit()
+						.putStringSet(ANNOUNCEITEMPOSITION, announceItemPosition).commit();//保存到配置文件中
+				/*Iterator<String> it = announceItemPosition.iterator();//测试使用
+				while (it.hasNext()) {
+					Log.e("==dianji==", it.next());
+				}
+				Map map=mSharedPreferences.getAll();
+				Set set=map.keySet();
+				Iterator iterator=set.iterator();
+				while(iterator.hasNext()){
+					Log.e("=====", iterator.next()+"");
+				}*/
+				Announcement data = (Announcement) parent.getAdapter().getItem(
+						position);
+				Bundle bundle = new Bundle();
 				bundle.putString(Constant.ANNOUNCEMENTID, data.getId());
-				Intent intent=new Intent(activity,PersonalAnnouncementDetailActivity.class);
+				Intent intent = new Intent(activity,
+						PersonalAnnouncementDetailActivity.class);
 				intent.putExtras(bundle);
 				startActivity(intent);
 			}
 		});
 	}
-	
+
 	private void initData() {
 		startProgressDialog("正在加载中...");
-		Request request = httpEngine.createRequest(ServicesConfig.PERSONAL_ANNOUNCEMENT);
-		Call call=httpEngine.createRequestCall(request);
+		Request request = httpEngine
+				.createRequest(ServicesConfig.PERSONAL_ANNOUNCEMENT);
+		Call call = httpEngine.createRequestCall(request);
 		call.enqueue(new Callback() {
-			
+
 			@Override
 			public void onResponse(Response response) throws IOException {
-				Message msg=new Message();
-				if(response.isSuccessful()){
-					msg.what=StatusCode.ANNOUNCEMENT_SUCCESS;
-					msg.obj=response.body().string();
-				}else{
-					msg.what=StatusCode.RESPONSE_SERVER_EXCEPTION;
-					msg.obj="服务器异常";
+				Message msg = new Message();
+				if (response.isSuccessful()) {
+					msg.what = StatusCode.ANNOUNCEMENT_SUCCESS;
+					msg.obj = response.body().string();
+				} else {
+					msg.what = StatusCode.RESPONSE_SERVER_EXCEPTION;
+					msg.obj = "服务器异常";
 				}
 				handler.sendMessage(msg);
 			}
-			
+
 			@Override
 			public void onFailure(Request arg0, IOException arg1) {
-				Message msg=new Message();
-				msg.what=StatusCode.RESPONSE_NET_FAILED;
-				msg.obj="网络异常";
+				Message msg = new Message();
+				msg.what = StatusCode.RESPONSE_NET_FAILED;
+				msg.obj = "网络异常";
 				handler.sendMessage(msg);
 			}
 		});
